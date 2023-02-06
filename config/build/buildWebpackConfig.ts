d054995984be461f431928179bac1e0820ca6686
MaksDubina <mgd8832@gmail.com>
2023-02-06 17:38:29 +0700

    add config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,28 @@
+import {BuildOptions} from "./types/config";
+import webpack from "webpack";
+import path from "path";
+import {buildPlagins} from "./buildPlagins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions) :webpack.Configuration{
+    const {mode, paths, isDev} = options
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlagins(options),
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map': undefined,
+        devServer: isDev ?buildDevServer(options): undefined,
+
+    }
+}
