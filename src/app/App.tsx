f62e874f3e2716efd085d0d2461202b452f014db
MaksDubina <mgd8832@gmail.com>
2023-02-13 19:03:42 +0700

    add i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
@@ -7,19 +7,21 @@ import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
 
 
-
 const App = () => {
 
     const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback=''>
+                <Navbar/>
 
-            <Navbar/>
-            <div className='content-page'>
-                <Sidebar/>
-                <AppRouter/>
-            </div>
+                <div className='content-page'>
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
+
 
 
         </div>
