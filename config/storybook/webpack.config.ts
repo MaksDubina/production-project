1b5d6976d211f9fa2e51e7fe00e210905301802e
MaksDubina <mgd8832@gmail.com>
2023-03-10 03:55:41 +0700

    add main pipeline github

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,4 +1,4 @@
-import webpack from 'webpack';
+import webpack, { RuleSetRule } from 'webpack';
 import path from 'path';
 import { BuildPaths } from '../build/types/config';
 import { buildCssLoaders } from '../build/loaders/buildCssLoaders';
@@ -14,6 +14,19 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config.resolve.extensions.push('.ts', '.rsx');
 
     config.module.rules.push(buildCssLoaders(true));
+    // eslint-disable-next-line no-param-reassign
+    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+        if (/svg/.test(rule.test as string)) {
+            return { ...rule, exclude: /\.svg$/i };
+        }
+
+        return rule;
+    });
+
+    config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    });
 
     return config;
 };
